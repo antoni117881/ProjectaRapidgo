ed092977f0b735d3fdcda26cfd957c8bad0a15cb
antoni117881 <antoni_pereblasco@ceroca.cat>
2025-02-10 19:02:13 +0100

    Funcionalidades del menu acabadas

--- Index.php
+++ Index.php
@@ -7,6 +7,28 @@ $action = $_GET['action'] ?? null; //es mas robusta ?? si es null
 
 switch ($action) { //aqui solo apuntamos a controladores y resource no a modulos
    
+    case 'Login':
+        include __DIR__.'/Resource_lgin.php';
+        break;
+    case 'registro':
+        include __DIR__.'/Resource_registro.php';
+         break;
+             
+    case 'Restaurantes':
+        include __DIR__.'./Vista/restaurantes.php';
+          break;
+          case 'Ofertas':
+            include __DIR__.'./Vista/Ofertas.php';
+              break;
+    case 'Menus':
+        include __DIR__.'./Vista/Menus.php';
+        break;
+        case 'Usuario':
+            include __DIR__.'./Resource_usuario.php';
+            break;
+    case 'Menu':
+         include __DIR__.'../RegistroMenu.php';
+          break;   
     default:
         include __DIR__.'../RegistroMenu.php';
         break;
---
+++ Vista/Menus.php
@@ -0,0 +1,8 @@
+<html>
+    <h1>
+        Estas en la pagina de menus
+    </h1>
+    <ul>
+         <li><a href="?action=Menu">Volver al menu</a></li>
+    </ul>   
+</html>
