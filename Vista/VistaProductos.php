a8697aa01c37170fa95e51f98a7fe9430c957d8d
antoni117881 <antoni_pereblasco@ceroca.cat>
2025-02-20 17:46:11 +0100

    navegacion estilo acabado y estilo de productos

--- Vista/VistaProductos.php
+++ Vista/VistaProductos.php
@@ -19,7 +19,6 @@
                                 <h2 class="nombre-producto"><?php echo $producto['Nombre']; ?></h2>
                                     <img class="imagen-producto" ><img src="<?php echo $producto['Imagen']; ?>" alt="<?php echo $producto['Nombre']; ?>" width=' 300px' height ='300px'/>
                                     <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
                                     <p class="descripcion"><?php echo $producto['Descripcion']; ?></p>
                                     <p class="precio">Precio: $<?php echo number_format($producto['PrecioUnidad'], 2); ?></p>
                                     <p class="id_producto">Id Producto: <?php echo $producto['ID']; ?></p>
